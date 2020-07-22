9b9caf2287a9866cb81b4fb3286a5430c322d8b6
QA Hoang <qahoang317@gmail.com>
2020-07-21 22:41:47 -0700

    remove .github and CHANGELOG.md

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ require(`dotenv`).config({
 const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
+  pathPrefix: "/gatsby-blog",
   siteMetadata: {
     siteTitle: "QA Hoang",
     siteTitleAlt: `QA Blog`,
